f22dedcdd43def35365f1f2dd736594905e17848
NikolaiTeslovich <nekrutnikolai@nikolais-mbp.lan>
2021-03-26 13:33:47 -0400

    made navbar resizable

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -61,19 +61,19 @@ class App extends Component {
       <div className="App">
 
       {/* The NavigationBar */}
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" expand="lg">
         <Navbar.Brand href="#home">Exercise Token</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">About</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          {/*<NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown>*/}
             <DarkMode />
           </Nav>
           <Navbar.Text>
